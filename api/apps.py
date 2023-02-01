48b3563a1c3e6ade33d8fe7fa7e1bbe4abc9d376
IllTamer <78360471+IllTamer@users.noreply.github.com>
2023-02-02 02:15:21 +0800

    Add init method

--- api/apps.py
+++ api/apps.py
@@ -1,6 +1,24 @@
-from django.apps import AppConfig
+import os
 
+from django.apps import AppConfig
+from threading import Lock
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+    lock = Lock()
+    
+    def ready(self) -> None:
+        ApiConfig.lock.acquire()
+        try:
+            run_once = os.environ.get('CMDLINERUNNER_RUN_ONCE') 
+            if run_once is not None: return
+            os.environ['CMDLINERUNNER_RUN_ONCE'] = 'True' 
+            Init() 
+        except Exception as e:
+            print(e)
+        finally:
+            ApiConfig.lock.release()
+
+def Init():
+    pass
